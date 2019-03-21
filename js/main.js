120bf808f3dbce1b3ea06645d9a8d746e7128463
GarethBarnett <osd@hotmail.co.nz>
2019-03-21 23:33:32 +1300

    +Ham Menu +splash css

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 
-   var pathEls = document.querySelectorAll('path');
+// background animation
 
+let pathEls = document.querySelectorAll('path');
 
-
-
-for (var i = 0; i < pathEls.length; i++) {
-  var pathEl = pathEls[i];
-  var offset = anime.setDashoffset(pathEl);
+for (let i = 0; i < pathEls.length; i++) {
+  let pathEl = pathEls[i];
+  let offset = anime.setDashoffset(pathEl);
   pathEl.setAttribute('stroke-dashoffset', offset);
   anime({
     targets: pathEl,
@@ -20,6 +19,21 @@ for (var i = 0; i < pathEls.length; i++) {
   });
 }
   
+
+// Hamburger Menu animation
+
+
+const menu = document.getElementsByClassName("menu-btn")[0];
+   menu.onclick = function() {
+        menu.classList.toggle('is-active');
+        document.getElementById("menu").classList.toggle('enter');
+    //     document.getElementById("menu").classList.add('bounceInRight')
+
+    //     setTimeout(function() {
+    //   document.getElementById("menu").classList.remove('bounceInRight')
+    // }, 1000);
+        
+};
 
 
 
